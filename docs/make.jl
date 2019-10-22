fee22fc0e7bb83bd1a8260441d18ee0dc4d62fc4
benjaminwsebastian <bensebastian@mac.com>
2019-10-22 12:02:36 +0000

    fixed error

--- docs/make.jl
+++ docs/make.jl
@@ -19,4 +19,4 @@ end
 
 makedocs(sitename = "HMM2d", modules = [HMM2d], pages = ["index.md", "Manual" => ["placeholder.md"], "theory.md" ])
 
-deploydocs( repo = "github.com/benjaminwsebastian/HMM2d.jl.git )
+deploydocs( repo = "github.com/benjaminwsebastian/HMM2d.jl.git" )
