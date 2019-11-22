44a2b828777e6cd64b90f5819d4f5e30320d8a94
Benjamin Sebastian <44763636+benjaminwsebastian@users.noreply.github.com>
2019-11-22 22:49:39 +0300

    Update HMM2d.jl

--- src/HMM2d.jl
+++ src/HMM2d.jl
@@ -4,8 +4,8 @@ module HMM2d
 2 Dimensional Hidden Markov Models
 =#
 
-include("hmm2d.jl")
-include("viterbi.jl")
-include("mle.jl")
+#include("hmm2d.jl")
+#include("viterbi.jl")
+#include("mle.jl")
 
 end # module
